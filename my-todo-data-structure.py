709ff75ce5f6f4c84ac435643e72e733e16043c9
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-09 15:29:14 +0200

    (feat): checker function is done

--- my-todo-data-structure.py
+++ my-todo-data-structure.py
@@ -25,32 +25,55 @@ def print_help():
 def list_element():
     my_file = open('my-todo.txt', 'r')
     my_line = my_file.read()
-    #print(my_line)
     my_file.close()
     view_todo()
 
-        
+            
+    
 def add_element():
-    add_file = open('my-todo.txt', 'r+')
-    add_file.write('\n0;' + sys.argv[2].rstrip())
-    add_line = add_file.readlines()
-    print(add_line)
+    add_file = open('my-todo.txt', 'a')
+    add_element = '\n0;' + sys.argv[2].rstrip()
+    add_line = add_file.writelines(add_element)
     add_file.close()
+    view_todo()
 
 
 def remove_element():
     remove_file = open('my-todo.txt', 'r+')
-    remove_lines = remove_file.readlines()
-    print(remove_lines)
+    remove_list = remove_file.readlines()
+    remove_index = int(sys.argv[2])-1 # commadline arguments are strings always 
+    item_to_delet = ''
+    for i in range(len(remove_list)): #while looping through a list it is not recommended to delete element until the loop finished
+        if i == remove_index:
+            item_to_delet = remove_list[i]
+    remove_list.remove(item_to_delet) #remove method delet the string but not the index of an element
+    #print(remove_list)
     remove_file.close()
-
-
+    remove_file = open('my-todo.txt', 'w')
+    for i in remove_list:
+        remove_file.write(i)
+    remove_file.close()
+    view_todo()
+    
+    
+    
 def complete_element():
     complete_file = open('my-todo.txt', 'r+')
     complete_lines = complete_file.readlines()
-    print(complete_lines)
+    checker_index = int(sys.argv[2])-1
+    for i in range(len(complete_lines)):
+        if i == checker_index:
+            split_item = complete_lines[i].split(';')
+            if split_item[0] =='0':
+                split_item[0] = '1'
+                split_item = ';'.join(split_item)
+                complete_lines[i] = split_item
     complete_file.close()
-
+    complete_file = open('my-todo.txt', 'w')
+    for i in complete_lines:
+        complete_file.write(i)
+    complete_file.close()
+    view_todo()
 
 def view_todo():
     view_file = open('my-todo.txt', 'r')
@@ -61,9 +84,7 @@ def view_todo():
             print(number, '[x]', lines[1])
         else:
             print(number, '[ ]', lines[1])
-        number += 1
+            number += 1
     view_file.close()
-
-
 
 different_inputs()
